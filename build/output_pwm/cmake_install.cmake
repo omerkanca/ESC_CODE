c1dafaf9e3fddd21ce7867825cf3b681983f209b
omerkanca <omeryakupkanca67@gmail.com>
2025-05-20 22:42:55 +0300

    name change

--- build/output_pwm/cmake_install.cmake
+++ build/output_pwm/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/omery/OneDrive/Desktop/pico_a
+# Install script for directory: C:/Users/omery/OneDrive/Desktop/pico_a/output_pwm
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,24 +37,3 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Program Files/Raspberry Pi/Pico SDK v1.5.1/gcc-arm-none-eabi/bin/arm-none-eabi-objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/Users/omery/OneDrive/Desktop/pico_a/build/pico_extras/cmake_install.cmake")
-  include("C:/Users/omery/OneDrive/Desktop/pico_a/build/pico-sdk/cmake_install.cmake")
-  include("C:/Users/omery/OneDrive/Desktop/pico_a/build/main/cmake_install.cmake")
-  include("C:/Users/omery/OneDrive/Desktop/pico_a/build/output_pwm/cmake_install.cmake")
-  include("C:/Users/omery/OneDrive/Desktop/pico_a/build/deneme/cmake_install.cmake")
-  include("C:/Users/omery/OneDrive/Desktop/pico_a/build/back/cmake_install.cmake")
-
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/omery/OneDrive/Desktop/pico_a/build/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
