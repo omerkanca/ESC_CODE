6f87a5b008b8b02678ab899e2346061262257fcb
omerkanca <omeryakupkanca67@gmail.com>
2025-05-20 22:31:58 +0300

    aciklamalar

--- blink/blink.c
+++ blink/blink.c
@@ -18,9 +18,9 @@
 #define B_LOW 17
 #define C_LOW 18
 
-#define St_speed 50
+#define St_speed 50 // Satart speed
 
-#define NUM 0
+#define NUM 0   // Tİmer kısımları
 #define IRQ TIMER_IRQ_0
 static volatile bool flag = true;
 
@@ -30,32 +30,32 @@ uint adc_2 = 0;
 
 int step=1; // 6 step fonksiyonunda adım belirtmek için
 int speed = 0;  // PWM çıkışların son halidir
-uint StepTime = 0;
-bool Drm = 0;
-uint16_t elapsed = 0;
-uint16_t last_time = 0;
+bool Drm = 0;   // Hareket durum değişkeni
+
+uint16_t elapsed = 0;   // İki adım arası geçen sürenin değişkeni
+uint16_t last_time = 0; // Bir önceki adımın zamanını belirtir
 
 volatile float duty_cycle = 0.0f;   // Duty değeri için ortak değişken ( volatile diğer çekirdekten değiştirilebilir )
 
 // Kesme
 void Kesme() {
-    hw_clear_bits(&timer_hw->intr, 1u << NUM);
-    flag = true;
+    hw_clear_bits(&timer_hw->intr, 1u << NUM);  // Kesme bayrağı temizlenir
+    flag = true;    // Kesme tamamlandı diye bayrak 1 yapılır
 }
-// Alarmı başlatan fonksiyon
+// 
 void start_min(uint32_t delay_us) {
-    uint64_t now = timer_hw->timerawl;
-    timer_hw->alarm[NUM] = (uint32_t)(now + delay_us);
+    uint64_t now = timer_hw->timerawl;  // Şu anki zaman
+    timer_hw->alarm[NUM] = (uint32_t)(now + delay_us);  // Şu anki zamandan 1 us saniye sonra kesme yapıyor
 }
-void init_alarm_system() {
-    hw_set_bits(&timer_hw->inte, 1u << NUM);
-    irq_set_exclusive_handler(IRQ, Kesme);
-    irq_set_enabled(IRQ, true);
+void init_system() {
+    hw_set_bits(&timer_hw->inte, 1u << NUM);    // Kesme izni verilir 
+    irq_set_exclusive_handler(IRQ, Kesme);  // IRQ için ISR tanımlanır
+    irq_set_enabled(IRQ, true); // IRQ kesme hattı etkinleştirilir
 }
 
 // Faz anahtarlama
 void switch_phase(int step, int speed) {
-    switch (step) {
+    switch (step) { // Adım değişkenine göre tanımlı anahtarlamalar yapılır
         case 1:
             gpio_put(A_LOW, 1);
             gpio_put(C_LOW, 1);
@@ -110,16 +110,16 @@ void switch_phase(int step, int speed) {
 // Duty cycle ölçme fonksiyonu
 float measure_duty_cycle(uint gpio) {
     gpio_set_dir(gpio, GPIO_IN);
-    while (!gpio_get(gpio)); // LOW → HIGH
+    while (!gpio_get(gpio)); // LOW → HIGH (Yükselen kenar)
     absolute_time_t t1 = get_absolute_time();
-    while (gpio_get(gpio));  // HIGH → LOW
+    while (gpio_get(gpio));  // HIGH → LOW (Alçalan kenar)
     absolute_time_t t2 = get_absolute_time();
-    while (!gpio_get(gpio)); // LOW → HIGH (next)
+    while (!gpio_get(gpio)); // LOW → HIGH (next) (sonraki yükselen kenar, periyot hesaplama için)
     absolute_time_t t3 = get_absolute_time();
 
-    float high_time = absolute_time_diff_us(t1, t2);
-    float period = absolute_time_diff_us(t1, t3);
-    return high_time / period;
+    float high_time = absolute_time_diff_us(t1, t2);    // Yüksek kalma süresi
+    float period = absolute_time_diff_us(t1, t3);   // Periyot süresi 
+    return high_time / period;  // Duty oranı döndürülür
 }
 
 // 2. çekirdekte çalışacak fonksiyon
@@ -160,11 +160,11 @@ int ortalama(int step){
     adc_1 = adc_read();
     adc_select_input(2);
     adc_2 = adc_read();
-    int deg ;
-    switch (step)
+    int deg ;   // Değer değişkeni 
+    switch (step)   //  Step durumuna göre bi sonraki adımlar için gereken koşul kontrolü
     {
     case 1:
-    deg = (adc_0 + adc_1)/2 - adc_2;
+    deg = (adc_0 + adc_1)/2 - adc_2;    // Güç verilen bacakların ortalamsı alınır ve boştaki bacak ile fark alınır
         break;
     case 2:
     deg = (adc_0 + adc_2)/2 - adc_1;        
@@ -189,19 +189,19 @@ int ortalama(int step){
 
 //  Motor start fonk
 void START() {
-    StepTime = 15000;
-    while (StepTime > 1500)
+    uint StepTime = 15000;  // Başlangıç zaman değeri
+    while (StepTime > 1500) // Start kodunun sınırlandırması
     {
-        if (flag) {
+        if (flag) { // Kesme varsa bir sonraki adıma geç
             flag = false;
-            switch_phase(step, St_speed);
+            switch_phase(step, St_speed);   // Başlangıç hızını referans alır 
             step ++;
             if (step > 6 ) step = 1; 
             StepTime = StepTime-(StepTime/50);
-            start_min(StepTime);
+            start_min(StepTime);    // Kesme işlemi için süre belirtilir
         }
         speed = (duty_cycle-0.05f) * 10000.0f;   // speed değişkenini duty değerine göre sürekli günceller
-        if(speed < St_speed) break;
+        if(speed < St_speed) break; // Eğer hız başlangıç hızından düşükse motor durur
     }
     
 }
@@ -210,7 +210,7 @@ int main() {
     
     multicore_launch_core1(core1_entry);    // 2. çekirdeği başlatır ve core1_entry fonksiyonunu başlatır
 
-    init_alarm_system();
+    init_system();  //  Kesme fonksiyonu çalıştırılır
 
     adc_init(); // ADC donanımını çalıştırır
     adc_gpio_init(26);  // ADC pinleri seçilir
@@ -228,22 +228,22 @@ int main() {
     setup_pwm(C_HIGH);
     
     while (true) {
-        if(Drm == 0) switch_phase(step, 0);
+        if(Drm == 0) switch_phase(step, 0); // Harekete geçilmediği süreçte motoru durdur
         if(ortalama(step)){ // adım kontrol
 
-            uint16_t now = (uint16_t)timer_hw->timerawl;    // Adımlar arası zaman ölçümü
+            uint16_t now_1 = (uint16_t)timer_hw->timerawl;    // Adımlar arası zaman ölçümü
             if (last_time != 0) {  // eğer daha önce bir zaman varsa
-                elapsed = now - last_time;
+                elapsed = now_1 - last_time;    // Geçen zaman
             }
-            last_time = now;
+            last_time = now_1;  // Ölçülen vakit eski vakte kaydedilir
 
-            step ++;
+            step ++;    // Adım arttırılır
             if (step > 6 ) step = 1;   // Step değişkenini belli aralıkta döngüde tutar
         }
         speed = (duty_cycle-0.05f) * 10000.0f;   // speed değişkenini duty değerine göre sürekli günceller
-        if (Drm == 1) switch_phase(step, speed);
-        if (speed < St_speed) Drm = 0;
-        if (Drm == 0 && speed > St_speed)
+        if (Drm == 1) switch_phase(step, speed);    // Hareket varsa motor hız değişkenince çalışır
+        if (speed < St_speed) Drm = 0;  // Hız min hızdan düşükse hareket durumu 0 yapılır
+        if (Drm == 0 && speed > St_speed)   // Durum sıfırken hız başlangıç hızından büyükse durum 1 yapılır 
         {
             START();
             Drm = 1;
